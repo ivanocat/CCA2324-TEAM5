6cf5ba7cf2710e8e2451c792b53f7356041c5eff
root <root@ip-10-0-101-48.ec2.internal>
2024-01-13 13:31:40 +0000

    Install Node Exporter command instructions in the ec2 user Data

--- IaC/scripts/ec2-userdata.sh
+++ IaC/scripts/ec2-userdata.sh
@@ -6,3 +6,17 @@ yum install -y httpd
 systemctl start httpd
 systemctl enable httpd
 echo "<h1>Hello World from $(hostname -f)</h1>" > /var/www/html/index.html
+
+sudo useradd --no-create-home node_exporter
+
+wget https://github.com/prometheus/node_exporter/releases/download/v1.0.1/node_exporter-1.0.1.linux-amd64.tar.gz
+tar xzf node_exporter-1.0.1.linux-amd64.tar.gz
+sudo cp node_exporter-1.0.1.linux-amd64/node_exporter /usr/local/bin/node_exporter
+rm -rf node_exporter-1.0.1.linux-amd64.tar.gz node_exporter-1.0.1.linux-amd64
+
+sudo cp node-exporter.service /etc/systemd/system/node-exporter.service
+
+sudo systemctl daemon-reload
+sudo systemctl enable node-exporter
+sudo systemctl start node-exporter
+sudo systemctl status node-exporter
