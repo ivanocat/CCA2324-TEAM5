35adad787d4217711cb194ebf12ce82aa7e15dab
Ivan <ivano.es@gmail.com>
2024-01-20 20:31:43 +0100

    Write values to node exporter service file

--- IaC/scripts/ec2-userdata.sh
+++ IaC/scripts/ec2-userdata.sh
@@ -14,7 +14,18 @@ tar xzf node_exporter-1.0.1.linux-amd64.tar.gz
 sudo cp node_exporter-1.0.1.linux-amd64/node_exporter /usr/local/bin/node_exporter
 rm -rf node_exporter-1.0.1.linux-amd64.tar.gz node_exporter-1.0.1.linux-amd64
 
-sudo cp node-exporter.service /etc/systemd/system/node-exporter.service
+echo "[Unit]
+Description=Prometheus Node Exporter Service
+After=network.target
+
+[Service]
+User=node_exporter
+Group=node_exporter
+Type=simple
+ExecStart=/usr/local/bin/node_exporter
+
+[Install]
+WantedBy=multi-user.target" > /etc/systemd/system/node-exporter.service
 
 sudo systemctl daemon-reload
 sudo systemctl enable node-exporter
