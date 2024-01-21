9b661526698a22b1f211aa4fdc143b7c43aaaf1e
root <root@ip-10-0-101-25.ec2.internal>
2024-01-21 21:25:23 +0000

    Odoo application

--- IaC/scripts/ec2-userdata.sh
+++ IaC/scripts/ec2-userdata.sh
@@ -1,11 +1,62 @@
 #!/bin/bash
-# Use this for your user data (script from top to bottom)
-# install httpd (Linux 2 version)
-yum update -y
-yum install -y httpd
-systemctl start httpd
-systemctl enable httpd
-echo "<h1>Hello World from $(hostname -f)</h1>" > /var/www/html/index.html
+
+# Update the system
+sudo yum update -y
+
+# Install dependencies
+sudo yum install -y docker
+
+# Start and enable the Docker service
+sudo systemctl start docker
+sudo systemctl enable docker
+
+# Install Docker Compose
+sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+# Create a directory for Docker Compose configuration files
+mkdir ~/odoo-docker
+cd ~/odoo-docker
+
+# Create a docker-compose.yml file with the provided content
+echo "version: '3.1'
+services:
+  web:
+    image: odoo:16.0
+    depends_on:
+      - db
+    ports:
+      - '8069:8069'
+  db:
+    image: postgres:15
+    environment:
+      - POSTGRES_DB=postgres
+      - POSTGRES_PASSWORD=odoo
+      - POSTGRES_USER=odoo
+" > docker-compose.yml
+
+# Start the containers with Docker Compose without prompting for confirmation
+sudo docker-compose up -d
+
+# Install Nginx
+sudo yum install -y nginx
+
+# Configure Nginx on the host system
+echo "server {
+    listen 80;
+    server_name localhost;
+
+    location / {
+        proxy_pass http://localhost:8069;
+        proxy_set_header Host \$host;
+        proxy_set_header X-Real-IP \$remote_addr;
+        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto \$scheme;
+    }
+}" | sudo tee /etc/nginx/conf.d/odoo.conf > /dev/null
+
+# Restart Nginx to apply the changes
+sudo systemctl restart nginx
 
 sudo useradd --no-create-home node_exporter
 
