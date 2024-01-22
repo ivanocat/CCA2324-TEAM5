2f48640077814808990d869f03852108db050b94
Xavier González Mur <146945906+xgonzalezmur@users.noreply.github.com>
2024-01-22 17:41:53 +0100

    Delete IaC_(alternative) directory

--- IaC_(alternative)/asg/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "auto_scaling_group_name" {
-  description = "Auto scaling group name"
-  value = aws_autoscaling_group.application_asg.name
-}
-
-output "launch_template_id" {
-  description = " launch template id"
-  value = aws_launch_template.application_lt.id
-}
